eb505c48f139aec079b2b22640ca9a9b49a5bf5c
Christopher Walker <cwalker@polytrope.com>
2019-03-19 09:31:15 -0400

    Initial Commit
    FIRST POST!!!!

---
+++ index.js
@@ -0,0 +1,45 @@
+var Botkit = require('botkit')
+var math = require('mathjs')
+
+console.log("Booting flumber bot")
+
+var controller = Botkit.slackbot({
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  clientSigningSecret: process.env.clientSigningSecret,
+  scopes: ['bot', 'chat:write:bot'],
+});
+
+controller.setupWebserver(process.env.port,function(err,webserver) {
+    controller.createWebhookEndpoints(controller.webserver);
+    controller.createOauthEndpoints(controller.webserver);
+});
+
+var parser = math.parser()
+
+controller.hears('.*', 'direct_mention,direct_message', (bot, message) => {
+  new Promise((resolve, reject) => {
+    console.log("Evaluating Message:")
+    console.log(message)
+
+    var reply = ""
+
+    try {
+      var expression = message.text
+      var result = parser.eval(expression)
+      reply = "anser :) --> " + result
+    }
+    catch (error) {
+      console.log("ERROR:\n"+error)
+      reply = "i dunt unerstand :(\n\n"+error
+    }
+
+    resolve(reply)
+  })
+  .then((res) => {
+    return bot.reply(message, res);
+  })
+  .error((error) => {
+    return bot.reply(message, error);
+  })
+});
